1d4470fd613a74f7f5a0a10713a836141e9fb7a5
Laurent Peuch <cortex@worlddomination.be>
2015-06-05 07:28:31 +0200

    [enh] add more commands to logger

--- caprice/core/logging.py
+++ caprice/core/logging.py
@@ -2,6 +2,32 @@ import sys
 
 
 class Logger(object):
+    # "\033[*;**(;**)m"
+    # "\033[*;**(;**)m"
+    DEFAULT = "\033[0m"
+    OK = "\033[0;32m"
+    QUESTION = "\033[0;36m"
+    ACTION = "\033[1;36m"
+
+
+    def action(self, message):
+        sys.stdout.write(self.ACTION)
+        sys.stdout.write(message.lstrip())
+        sys.stdout.write(self.DEFAULT)
+        sys.stdout.write("\n")
+
+    def question(self, message):
+        sys.stdout.write(self.QUESTION)
+        sys.stdout.write(message.lstrip())
+        sys.stdout.write(self.DEFAULT)
+        sys.stdout.write("\n")
+
+    def ok(self, message):
+        sys.stdout.write(self.OK)
+        sys.stdout.write(message.lstrip())
+        sys.stdout.write(self.DEFAULT)
+        sys.stdout.write("\n")
+
     def info(self, message):
         sys.stdout.write(message.lstrip())
         sys.stdout.write("\n")
